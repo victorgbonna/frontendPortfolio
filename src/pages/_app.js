51b00a23747e45a27a5803ba263f5158f73352ef
GreyHaired <victorgbonna@gmail.com>
2023-08-09 16:13:02 +0100

    adding gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
 import "../styles/globals.css";
-
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,6 +10,25 @@ function MyApp({ Component, pageProps }) {
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"/>
         {/* <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link> */}
       </Head>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-TM98X96286" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-TM98X96286');
+        `}
+      </Script>
+      {/* <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-TM98X96286"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-TM98X96286');
+      </script> */}
       <Component {...pageProps}/>
       <Analytics />
     </>
